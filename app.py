f080075918e76b9bdb046fb9bae3cb246f67a9b8
kashish <kashishbaddgujar@gmail.com>
2026-02-24 08:12:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def index():
     return render_template("index.html", prediction=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
